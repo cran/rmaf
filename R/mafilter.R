0982e32ba923c0375f237c458abaef452f3f8e75
Debin Qiu <debinqiu@uga.edu>
2015-04-14 00:00:00 +0000

    version 3.0.1

--- R/mafilter.R
+++ R/mafilter.R
@@ -50,29 +50,31 @@ ma.filter <- function (x, q = NULL, seasonal = FALSE, period = NULL,plot = TRUE)
   rt <- xt - mt
   result <- as.matrix(cbind(x, mt, if (seasonal) st, rt))
   colnames(result) <- c("data","trend",if (seasonal) "season","residual")
-  if (seasonal) {
-    op <- par(mfrow = c(2,2))
-    plot(result[,1],type = "l",xlab = "time",ylab = "data",
-         main = "original v.s fitted data")
-    lines(result[,2] + result[,3], col = 2)
-    plot(result[,2],type = "l", col = 3,xlab = "time",ylab = "estimated trend",
-         main = "plot of estimated trend")
-    plot(result[,3],type = "l", col = 4,xlab = "time",ylab = "seasonal index",
-         main = "plot of seasonal index")
-    plot(result[,4],type = "l", xlab = "time",ylab = "residuals",
-         main = "plot of residuals")
-    abline(h = 0, lty = 2, col = 5)
-    par(op)
-  }
-  else {
-    op <- par(mfrow = c(2,1))
-    plot(result[,1],type = "l",xlab = "time",ylab = "data",
-         main = "original v.s fitted data")
-    lines(result[,2],col = 2)
-    plot(result[,3],type = "l",xlab = "time",ylab = "residuals",
-         main = "plot of residuals")
-    abline(h = 0, lty = 2, col = 3)
-    par(op)
+  if (plot) {
+    if (seasonal) {
+      op <- par(mfrow = c(2,2))
+      plot(result[,1],type = "l",xlab = "time",ylab = "data",
+           main = "original v.s fitted data")
+      lines(result[,2] + result[,3], col = 2)
+      plot(result[,2],type = "l", col = 3,xlab = "time",ylab = "estimated trend",
+           main = "plot of estimated trend")
+      plot(result[,3],type = "l", col = 4,xlab = "time",ylab = "seasonal index",
+           main = "plot of seasonal index")
+      plot(result[,4],type = "l", xlab = "time",ylab = "residuals",
+           main = "plot of residuals")
+      abline(h = 0, lty = 2, col = 5)
+      par(op)
+    }
+    else {
+      op <- par(mfrow = c(2,1))
+      plot(result[,1],type = "l",xlab = "time",ylab = "data",
+           main = "original v.s fitted data")
+      lines(result[,2],col = 2)
+      plot(result[,3],type = "l",xlab = "time",ylab = "residuals",
+           main = "plot of residuals")
+      abline(h = 0, lty = 2, col = 3)
+      par(op)
+    }
   }
   return(result)
 }
